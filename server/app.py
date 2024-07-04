38a0675bcca0610dc02f42dbeba643c921ad357c
casey <caseymwangi31@gmail.com>
2024-07-04 16:07:30 +0300

    creating  a back end

--- server/app.py
+++ server/app.py
@@ -14,13 +14,58 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method =='GET':
+        messages = [message.to_dict() for message in Message.query.order_by(Message.created_at.asc()).all()]
+        return make_response(jsonify(messages), 200)
 
-@app.route('/messages/<int:id>')
+    elif request.method == 'POST':
+        data = request.get_json()
+        new_message = Message(
+            body=data.get('body'),
+            username=data.get('username'),
+            created_at=data.get('created_at', None),
+            updated_at=data.get('updated_at', None)
+        )
+
+        db.session.add(new_message)
+        db.session.commit()
+
+        new_message_dict = new_message.to_dict()
+        return make_response(jsonify(new_message_dict), 201)
+
+        
+
+@app.route('/messages/<int:id>', methods=['GET', 'PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter(Message.id==id).first()
+
+    if not message:
+        return make_response(jsonify({'message': 'Message not found'}), 404)
+
+    if request.method =='GET':
+        return make_response(jsonify(message.to_dict()), 200)
+
+    elif request.method == 'PATCH':
+        data = request.get_json()
+        for attr, value in data.items():
+            setattr(message, attr, value)
+
+        
+        db.session.commit()
+        return make_response(jsonify(message.to_dict()), 200)
+
+        
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+
+        response_body = {
+            "delete_successful": True,
+            "message": "Message deleted."
+        }
+        return make_response(jsonify(response_body), 200)
 
 if __name__ == '__main__':
     app.run(port=5555)
